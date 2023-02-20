c7c8add3398a2e92663ac39308b07b11e9a7cca2
Elena Sinelnikova <elle.sinelle@gmail.com>
2023-02-20 10:56:43 +0300

    task done

--- src/index.js
+++ src/index.js
@@ -39,28 +39,27 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     let tensArr = [];
-    for (let i = 0; i < expr.length; i += 10) {
+    for (let i=0; i<expr.length; i+=10) {
         tensArr.push(expr.slice(i, i + 10));
     }
-
-    let codedArr = [];
-    tensArr.forEach((item) => {
-        let codedSymb = '';
-        for (let i = 0; i < 10; i += 2) {
-            if (item[i] + item[i+1] == '10') {
-                codedSymb += '.';
-            } else if (item[i] + item[i+1] == '11') {
-                codedSymb += '-';
-            } else if (item[i] + item[i+1] == '00') {
-                codedSymb += ''
-            }
+    
+    let dotsArr = [];
+    for (let item of tensArr) {
+        let letterArr = [];
+        for (let i=0; i<item.length; i+=2) {
+            if (item[i] === '1' && item[i+1] === '0') {
+                letterArr.push('.');
+            } else if (item[i] === '1' && item[i+1] === '1') {
+                letterArr.push('-');
+            } else if (item[i] === '*') {
+                letterArr.push(' '); 
+                break;
+            } 
         }
-        codedArr.push(codedSymb);
-    })
-
-    let decodedArr = codedArr.map((item) => {
-        return MORSE_TABLE[item] || ' ';
-    })
+        dotsArr.push(letterArr.join(''));
+    }
+    
+    let decodedArr = dotsArr.map(unit => MORSE_TABLE[unit] || ' ');
 
     return decodedArr.join('');
 }
